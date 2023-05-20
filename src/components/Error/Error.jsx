2f8801b3ee7e86ca96bb0b6cc567d7f68617fc70
taniyamim <[jannatulhafsa500@gmail.com]>
2023-05-21 04:10:39 +0600

    error page animation added

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,8 +1,21 @@
+import { Link } from "react-router-dom";
+import './Error.css'
 
 const Error = () => {
     return (
-        <div>
-            <h3>error</h3>
+        <div className="error">
+            <div class="text">
+                <div>Looks Like you are lost in the space</div>
+                <h1>404</h1>
+                <hr/>
+                    <div>Page Not Found</div>
+                    <Link to='/'>Go To Home</Link>
+            </div>
+
+            <div class="astronaut">
+                <img src="https://images.vexels.com/media/users/3/152639/isolated/preview/506b575739e90613428cdb399175e2c8-space-astronaut-cartoon-by-vexels.png" alt="" class="src"/>
+            </div>
+          
         </div>
     );
 };
